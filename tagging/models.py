eae58c2471b84f5b7a6675c38566ae6f6d6fbb18
Konstantinos Bairaktaris <kbairak@transifex.com>
2022-08-31 09:40:35 +0300

    Remove import of python_2_unicode_compatible

--- tagging/models.py
+++ tagging/models.py
@@ -6,7 +6,6 @@ import urllib
 from django.db import models
 from django.db import connection
 from django.utils.encoding import smart_text
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
@@ -476,7 +475,6 @@ class TaggedItemManager(models.Manager):
 # Models #
 ##########
 
-@python_2_unicode_compatible
 class Tag(models.Model):
     """
     A tag.
@@ -500,7 +498,6 @@ class Tag(models.Model):
         return self.name
 
 
-@python_2_unicode_compatible
 class TaggedItem(models.Model):
     """
     Holds the relationship between a tag and the item being tagged.
